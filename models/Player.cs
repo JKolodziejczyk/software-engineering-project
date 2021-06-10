fae8481b15702507f9895450e65b9b46008d5c3c
Jakub Kołodziejczyk <jakubkolodziejczyk56@gmail.com>
2021-06-10 22:04:56 +0200

    removing some redundant code from player.cs

--- models/Player.cs
+++ models/Player.cs
@@ -1,63 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace RPG.models
 {
     public abstract class Player
     {
-        public string _class { get; }
-        public int _class_id { get; set; }
-        int[] stats { get; set; }
-        Item item1 { get; set; }
-        Item item2 { get; set; }
-        Item item3 { get; set; }
-        public List<Buff> _buffs { get; set; }
+        protected string _class { get; init; }
+        protected int _class_id { get; init; }
+        protected int[] stats { get; set; }
+        private Item item1 { get; set; }
+        private Item item2 { get; set; }
+        private Item item3 { get; set; }
+        private List<Buff> _buffs { get; set; }
 
         public void Change_stat(int stat_type, int value)
         {
-            this.stats[stat_type]+=value;
+            stats[stat_type] += value;
+            stats_min_max();
         }
+
 
         public void add_buff(Buff buff)
         {
             _buffs.Add(buff);
         }
 
-        public void Expiring_buffs()
+        protected abstract void stats_min_max();
+        private void Expiring_buffs()
         {
-            for (int i=0; i<this._buffs.Count; i++)
+            for (var i=0; i<this._buffs.Count; i++)
             {
                 if (_buffs[i].remaining_time() <= 0) _buffs.RemoveAt(i);
                 else _buffs[i].decrement_time();
             }
         }
+
+        private void apply_buffs()
+        {
+            foreach (var elem in _buffs)
+            {
+                for (var i = 0; i < 6; i++) stats[i] += elem.stats[i];
+            }
+        }
+
+        public void end_event()
+        {
+            Expiring_buffs();
+            apply_buffs();
+        }
         public abstract bool Change_item(Item new_item, int which_slot);
 
         public int[] DisplayStats()
         {
-            int[] Current = new int[7];
-            Current[0] = this.stats[0];
-            Current[1] = this.stats[1];
-            Current[2] = this.stats[2];
-            Current[3] = this.stats[3];
-            Current[4] = this.stats[4];
-            Current[5] = this.stats[5];
-            Current[6] = this.stats[6];
-            foreach (Buff elem in _buffs)
+            var Current = new int[7];
+            for (var i = 0; i < 7; i++)
             {
-                for (int i = 0; i < 6; i++) Current[i] += elem.stats[i];
+                Current[i] = this.stats[i];
             }
             if (item1 != null)
             {
-                for (int i = 0; i < 3; i++) Current[i] += item1._stats[i];
+                for (var i = 0; i < 3; i++) Current[i] += item1._stats[i];
             }
             if (item2 != null)
             {
-                for (int i = 0; i < 3; i++) Current[i] += item2._stats[i];
+                for (var i = 0; i < 3; i++) Current[i] += item2._stats[i];
             }
             if (item3 != null)
             {
-                for (int i = 0; i < 3; i++) Current[i] += item3._stats[i];
+                for (var i = 0; i < 3; i++) Current[i] += item3._stats[i];
             }
             /*
             Console.WriteLine("Atak: ",Current[0]);
@@ -73,14 +84,33 @@ namespace RPG.models
 
     public class Berserker : Player
     {
-        Weapon item1 { get; set; }
-        Weapon item2 { get; set; }
-        Lucky item3 { get; set; }
+        private Weapon item1 { get; set; }
+        private Weapon item2 { get; set; }
+        private Lucky item3 { get; set; }
         public Berserker()
         {
             item1 = null;
             item2 = null;
             item3 = null;
+            _class = "Berserker";
+            _class_id = 1;
+        }
+
+        protected override void stats_min_max()
+        {
+            if (stats[0] > 65) stats[0] = 65;
+            if (stats[1] > 35) stats[1] = 35;
+            if (stats[2] > 50) stats[2] = 50;
+            if (stats[3] > 100) stats[3] = 100;
+            if (stats[4] > 100) stats[4] = 100;
+            if (stats[5] > 100) stats[5] = 100;
+            if (stats[0] < 0) stats[0] = 0;
+            if (stats[1] < 0) stats[1] = 0;
+            if (stats[2] < 0) stats[2] = 0;
+            if (stats[3] < 0) stats[3] = 0;
+            if (stats[4] < 0) stats[4] = 0;
+            if (stats[5] < 0) stats[5] = 0;
+            if (stats[6] < 0) stats[6] = 0;
         }
         public override bool Change_item(Item new_item, int which_slot)
         {
@@ -88,106 +118,149 @@ namespace RPG.models
             {
                 case 1:
                 {
-                    if (new_item is Weapon) 
+                    if (new_item is Weapon item) 
                     {
-                        item1 =new_item as Weapon;
+                        item1 =item;
                         return true;
                     }
-                    else Console.WriteLine("Wybrałeś zły slot");
+                    Console.WriteLine("Wybrałeś zły slot");
                     return false;
                 }
                 case 2:
                 {
-                    if (new_item is Weapon)
+                    if (new_item is Weapon item)
                     {
-                        item2=new_item as Weapon;
+                        item2=item;
                         return true;
                     }
-                    else Console.WriteLine("Wybrałeś zły slot");
+                    Console.WriteLine("Wybrałeś zły slot");
                     return false;
                 }
                 case 3:
                 {
-                    if (new_item is Lucky)
+                    if (new_item is Lucky item)
                     {
-                        item3=new_item as Lucky;
+                        item3=item;
                         return true;
                     }
-                    else Console.WriteLine("Wybrałeś zły slot");
+                    Console.WriteLine("Wybrałeś zły slot");
                     return false;
                 }
                 case 0:
                     return true;
+                default:
+                {
+                    throw new InvalidOperationException();
+                }
             }
-
-            throw new InvalidOperationException();
         }
     }
 
     public class Knight : Player
     {
-        Weapon item1 { get; set; }
-        Armor item2 { get; set; }
-        Lucky item3 { get; set; }
+        private Weapon item1 { get; set; }
+        private Armor item2 { get; set; }
+        private Lucky item3 { get; set; }
 
         public Knight()
         {
             item1 = null;
             item2 = null;
             item3 = null;
+            _class = "Knight";
+            _class_id = 2;
         }
+
+        protected override void stats_min_max()
+        {
+            if (stats[0] > 50) stats[0] = 50;
+            if (stats[1] > 50) stats[1] = 50;
+            if (stats[2] > 50) stats[2] = 50;
+            if (stats[3] > 100) stats[3] = 100;
+            if (stats[4] > 100) stats[4] = 100;
+            if (stats[5] > 100) stats[5] = 100;
+            if (stats[0] < 0) stats[0] = 0;
+            if (stats[1] < 0) stats[1] = 0;
+            if (stats[2] < 0) stats[2] = 0;
+            if (stats[3] < 0) stats[3] = 0;
+            if (stats[4] < 0) stats[4] = 0;
+            if (stats[5] < 0) stats[5] = 0;
+            if (stats[6] < 0) stats[6] = 0;
+        }
+
         public override bool Change_item(Item new_item, int which_slot)
         {
             switch (which_slot)
             {
                 case 1:
                 {
-                    if (new_item is Weapon) 
+                    if (new_item is Weapon item) 
                     {
-                        item1 =new_item as Weapon;
+                        item1 =item;
                         return true;
                     }
-                    else Console.WriteLine("Wybrałeś zły slot");
+                    Console.WriteLine("Wybrałeś zły slot");
                     return false;
                 }
                 case 2:
                 {
-                    if (new_item is Armor)
+                    if (new_item is Armor item)
                     {
-                        item2=new_item as Armor;
+                        item2=item;
                         return true;
                     }
-                    else Console.WriteLine("Wybrałeś zły slot");
+                    Console.WriteLine("Wybrałeś zły slot");
                     return false;
                 }
                 case 3:
                 {
-                    if (new_item is Lucky)
+                    if (new_item is Lucky item)
                     {
-                        item3=new_item as Lucky;
+                        item3=item;
                         return true;
                     }
-                    else Console.WriteLine("Wybrałeś zły slot");
+                    Console.WriteLine("Wybrałeś zły slot");
                     return false;
                 }
                 case 0:
                     return true;
+                default:
+                {
+                    throw new InvalidOperationException();
+                }
             }
-
-            throw new InvalidOperationException();
         }
     }
 
     public class Thief : Player
     {
-        Weapon item1 { get; set; }
-        Lucky item2 { get; set; }
-        Lucky item3 { get; set; }
+        private Weapon item1 { get; set; }
+        private Lucky item2 { get; set; }
+        private Lucky item3 { get; set; }
         public Thief()
         {
             item1 = null;
             item2 = null;
             item3 = null;
+            _class = "Thief";
+            _class_id = 3;
+        }
+
+        protected override void stats_min_max()
+        {
+            if (stats[0] > 50) stats[0] = 50;
+            if (stats[1] > 35) stats[1] = 35;
+            if (stats[2] > 65) stats[2] = 65;
+            if (stats[3] > 100) stats[3] = 100;
+            if (stats[4] > 100) stats[4] = 100;
+            if (stats[5] > 100) stats[5] = 100;
+            if (stats[0] < 0) stats[0] = 0;
+            if (stats[1] < 0) stats[1] = 0;
+            if (stats[2] < 0) stats[2] = 0;
+            if (stats[3] < 0) stats[3] = 0;
+            if (stats[4] < 0) stats[4] = 0;
+            if (stats[5] < 0) stats[5] = 0;
+            if (stats[6] < 0) stats[6] = 0;
         }
         public override bool Change_item(Item new_item, int which_slot)
         {
@@ -195,39 +268,41 @@ namespace RPG.models
             {
                 case 1:
                 {
-                    if (new_item is Weapon) 
+                    if (new_item is Weapon item) 
                     {
-                        item1 =new_item as Weapon;
+                        item1 =item;
                         return true;
                     }
-                    else Console.WriteLine("Wybrałeś zły slot");
+                    Console.WriteLine("Wybrałeś zły slot");
                     return false;
                 }
                 case 2:
                 {
-                    if (new_item is Lucky)
+                    if (new_item is Lucky item)
                     {
-                        item2=new_item as Lucky;
+                        item2=item;
                         return true;
                     }
-                    else Console.WriteLine("Wybrałeś zły slot");
+                    Console.WriteLine("Wybrałeś zły slot");
                     return false;
                 }
                 case 3:
                 {
-                    if (new_item is Lucky)
+                    if (new_item is Lucky item)
                     {
-                        item3=new_item as Lucky;
+                        item3=item;
                         return true;
                     }
-                    else Console.WriteLine("Wybrałeś zły slot");
+                    Console.WriteLine("Wybrałeś zły slot");
                     return false;
                 }
                 case 0:
                     return true;
+                default:
+                {
+                    throw new InvalidOperationException();
+                }
             }
-
-            throw new InvalidOperationException();
         }
     }
     
